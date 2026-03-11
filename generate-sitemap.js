ad5f2c37a7b5980e56591a219773599ad48bc548
Robin <robin.p.winton@gmail.com>
2026-03-11 12:14:21 -0400

    Retry contact form with verified EmailJS credentials

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,10 +1,12 @@
 import fs from "fs";
+import path from "path";
 
 const baseUrl = "https://senseifood.com";
 
-const pages = [
-  "/"
-];
+const pages = ["/"];
+
+const publicDir = path.resolve("./public");
+const sitemapPath = path.join(publicDir, "sitemap.xml");
 
 let xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -20,10 +22,10 @@ pages.forEach((page) => {
 xml += `
 </urlset>`;
 
-if (!fs.existsSync("./dist")) {
-  fs.mkdirSync("./dist");
+if (!fs.existsSync(publicDir)) {
+  fs.mkdirSync(publicDir, { recursive: true });
 }
 
-fs.writeFileSync("./dist/sitemap.xml", xml);
+fs.writeFileSync(sitemapPath, xml, "utf8");
 
-console.log("Sitemap generated.");
+console.log(`Sitemap generated at: ${sitemapPath}`);
